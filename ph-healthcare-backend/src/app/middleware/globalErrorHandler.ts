c29e774ea99c933f344a725048b0fb662c508131
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-02-08 18:24:05 +0600

    zod installed & zod error handle done

--- ph-healthcare-backend/src/app/middleware/globalErrorHandler.ts
+++ ph-healthcare-backend/src/app/middleware/globalErrorHandler.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import status from "http-status";
-import { TErrorSources } from "../interface/error.interface";
+import { TErrorResponse, TErrorSources } from "../interface/error.interface";
+import z from "zod";
+import AppError from "../errorHelper/AppError";
+import { handleZodError } from "../errorHelper/handleZodError";
 
 export const globalErrorHandler = (
     err: any,
@@ -18,10 +21,41 @@ export const globalErrorHandler = (
     let message: string = "Internal Server Error";
     let stack: string | undefined = undefined;
 
-	res.status(statusCode).json({
-		success: false,
-		message,
-		errorSources,
-		stack,
-	});
+    if (err instanceof z.ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number;
+        message = simplifiedError.message;
+        errorSources = [...simplifiedError.errorSources];
+        stack = err.stack;
+    } else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+        stack = err.stack;
+        errorSources = [
+            {
+                path: "",
+                message: err.message,
+            },
+        ];
+    } else if (err instanceof Error) {
+        statusCode = status.INTERNAL_SERVER_ERROR;
+        message = err.message;
+        stack = err.stack;
+        errorSources = [
+            {
+                path: "",
+                message: err.message,
+            },
+        ];
+    }
+
+    const errorResponse: TErrorResponse = {
+        success: false,
+        message: message,
+        errorSources,
+        error: envVars.NODE_ENV === "development" ? err : undefined,
+        stack: envVars.NODE_ENV === "development" ? stack : undefined,
+    };
+
+    res.status(statusCode).json(errorResponse);
 };
--- ph-healthcare-backend/src/app/module/user/user.route.ts
+++ ph-healthcare-backend/src/app/module/user/user.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createDoctorZodSchema } from "./user.validation";
 
 const router = Router();
 
-router.post("/create-doctor", userController.createDoctor);
+router.post(
+    "/create-doctor",
+    validateRequest(createDoctorZodSchema),
+    userController.createDoctor,
+);
 // router.post("/create-admin", userController.createDoctor);
 // router.post("/create-super-admin", userController.createDoctor);
 
