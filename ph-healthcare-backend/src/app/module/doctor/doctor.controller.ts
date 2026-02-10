0205888c23d72925295591c1a9662fc792069e8b
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-02-10 18:37:15 +0600

    doctor CRUD operation done

--- ph-healthcare-backend/src/app/module/doctor/doctor.controller.ts
+++ ph-healthcare-backend/src/app/module/doctor/doctor.controller.ts
@@ -1,29 +1,65 @@
+import { Request, Response } from "express";
 import status from "http-status";
 import { catchAsync } from "../../shared/catchAsync";
 import { sendResponse } from "../../shared/sendResponse";
 import { doctorService } from "./doctor.service";
-import { NextFunction, Request, Response } from "express";
 
-const getAllDoctors = catchAsync(
-    async (req: Request, res: Response, next: NextFunction) => {
-        const result = await doctorService.getAllDoctors();
+const getAllDoctors = catchAsync(async (req: Request, res: Response) => {
+    const result = await doctorService.getAllDoctors();
 
-        sendResponse(res, {
-            httpStatusCode: status.OK,
-            success: true,
-            message: "Doctors fetched successfully",
-            data: result,
-        });
-    },
-);
+    sendResponse(res, {
+        httpStatusCode: status.OK,
+        success: true,
+        message: "Doctors fetched successfully",
+        data: result,
+    });
+});
 
-// const getDoctorById = catchAsync(
-//const updateDoctor = catchAsync(
-//const deleteDoctor = catchAsync(
+const getDoctorById = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
 
+    const doctor = await doctorService.getDoctorById(id as string);
+
+    sendResponse(res, {
+        httpStatusCode: status.OK,
+        success: true,
+        message: "Doctor fetched successfully",
+        data: doctor,
+    });
+});
+
+const updateDoctor = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const payload = req.body;
+
+    const updatedDoctor = await doctorService.updateDoctor(
+        id as string,
+        payload,
+    );
+
+    sendResponse(res, {
+        httpStatusCode: status.OK,
+        success: true,
+        message: "Doctor updated successfully",
+        data: updatedDoctor,
+    });
+});
+
+const deleteDoctor = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const result = await doctorService.deleteDoctor(id as string);
+
+    sendResponse(res, {
+        httpStatusCode: status.OK,
+        success: true,
+        message: "Doctor deleted successfully",
+        data: result,
+    });
+});
 export const doctorController = {
     getAllDoctors,
-    // getDoctorById,
-    // updateDoctor,
-    // deleteDoctor,
+    getDoctorById,
+    updateDoctor,
+    deleteDoctor,
 };
--- ph-healthcare-backend/src/app/module/doctor/doctor.route.ts
+++ ph-healthcare-backend/src/app/module/doctor/doctor.route.ts
@@ -1,12 +1,23 @@
 import { Router } from "express";
+import { Role } from "../../../generated/prisma/enums";
+import { checkAuth } from "../../middleware/checkAuth";
+import { validateRequest } from "../../middleware/validateRequest";
 import { doctorController } from "./doctor.controller";
+import { updateDoctorZodSchema } from "./doctor.validation";
 
 const router = Router();
 
-router.get("/", doctorController.getAllDoctors);
-// router.get("/:id", DoctorController.getDoctorById);
-// router.put("/:id", DoctorController.updateDoctor);
-//router.patch("/:id", DoctorController.updateDoctor);
-// router.delete("/:id", DoctorController.deleteDoctor);
+router.get("/",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    doctorController.getAllDoctors);
+router.get("/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    doctorController.getDoctorById);
+router.patch("/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validateRequest(updateDoctorZodSchema), doctorController.updateDoctor);
+router.delete("/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    doctorController.deleteDoctor);
 
-export const doctorRoute = router;
+export const doctorRoutes = router;
