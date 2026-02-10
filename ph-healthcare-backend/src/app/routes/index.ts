793fb4c3d9fd66645332aafd081eaaeb7a0b68f6
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-02-10 18:46:14 +0600

    admin crud operations done

--- ph-healthcare-backend/src/app/routes/index.ts
+++ ph-healthcare-backend/src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { specialtyRoute } from "../module/specialty/specialty.route";
 import { authRouter } from "../module/auth/auth.route";
 import { doctorRoutes } from "../module/doctor/doctor.route";
 import { userRoute } from "../module/user/user.route";
+import { adminRoutes } from "../module/admin/admin.route";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", authRouter);
 router.use("/specialties", specialtyRoute);
 router.use("/doctor", doctorRoutes);
 router.use("/user", userRoute);
+router.use("/admin", adminRoutes);
 
 export const IndexRoutes = router;
