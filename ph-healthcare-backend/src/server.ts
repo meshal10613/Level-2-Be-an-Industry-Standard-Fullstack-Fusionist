8aebc4f498b6f85ce6fd13a22d4dcfb31cfd801d
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-02-17 15:45:04 +0600

    chalk add to server.ts

--- ph-healthcare-backend/src/server.ts
+++ ph-healthcare-backend/src/server.ts
@@ -1,10 +1,15 @@
+import chalk from "chalk";
 import app from "./app";
 import { envVars } from "./app/config/env";
 
 (function bootstrap() {
     try {
         app.listen(envVars.PORT, () => {
-            console.log(`Server is running on http://localhost:${envVars.PORT}`);
+            console.log(
+                chalk.blue(
+                    `Server is running on http://localhost:${envVars.PORT}`,
+                ),
+            );
         });
     } catch (error) {
         console.error("Failed to start the server: ", error);
