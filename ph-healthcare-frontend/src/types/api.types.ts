0cdd6125da9cef0749edbc1db0f5961ac2ae9dba
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-03-01 14:04:44 +0600

    api types create

---
+++ ph-healthcare-frontend/src/types/api.types.ts
@@ -0,0 +1,18 @@
+export interface ApiResponse<TData> {
+    success: boolean;
+    message?: string;
+    data: TData;
+    meta?: PaginationMeta;
+}
+
+export interface PaginationMeta {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+}
+
+export interface ApiErrorResponse {
+    success: boolean;
+    message: string;
+}
