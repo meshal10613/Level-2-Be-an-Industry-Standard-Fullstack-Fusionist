8c59f49ef6a6170843ebbaacf850a025898c629e
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-03-03 21:21:07 +0600

    feat: enhance proxy middleware with role-based access control and redirection logic

--- ph-healthcare-frontend/src/proxy.ts
+++ ph-healthcare-frontend/src/proxy.ts
@@ -1,4 +1,11 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { jwtUtils } from "./lib/jwtUtils";
+import {
+    getDefaultDashboardRoute,
+    getRouteOwner,
+    isAuthRoute,
+    UserRole,
+} from "./lib/authUtils";
 
 export async function proxy(request: NextRequest) {
     try {
@@ -6,7 +13,73 @@ export async function proxy(request: NextRequest) {
         const accessToken = request.cookies.get("accessToken")?.value;
         const refreshToken = request.cookies.get("refreshToken")?.value;
 
-        
+        const decodedAccessToken =
+            accessToken &&
+            jwtUtils.verifyToken(
+                accessToken,
+                process.env.JWT_ACCESS_SECRET as string,
+            ).data;
+        const isValidAccessToken =
+            accessToken &&
+            jwtUtils.verifyToken(
+                accessToken,
+                process.env.JWT_ACCESS_SECRET as string,
+            ).success;
+
+        let userRole: UserRole | null = null;
+        if (decodedAccessToken) {
+            userRole = decodedAccessToken.role as UserRole;
+        }
+
+        const routerOwner = getRouteOwner(pathname);
+        const unifySuperAdminAndAdminRole =
+            userRole === "SUPER_ADMIN" ? "ADMIN" : userRole;
+        const isAuth = isAuthRoute(pathname);
+
+        //? Rule - 1 : User is logged in (has access token) and trying to access auth route -> allow
+        if (isAuth && isValidAccessToken) {
+            return NextResponse.redirect(
+                new URL(
+                    getDefaultDashboardRoute(userRole as UserRole),
+                    request.url,
+                ),
+            );
+        }
+
+        //? Rule-3 User trying to access Public route -> allow
+        if (routerOwner === null) {
+            return NextResponse.next();
+        }
+
+        //? Rule - 4 User is Not logged in but trying to access protected route -> redirect to login
+        if (!accessToken || !isValidAccessToken) {
+            const loginUrl = new URL("/login", request.url);
+            loginUrl.searchParams.set("redirect", pathname);
+            return NextResponse.redirect(loginUrl);
+        }
+
+        //? Rule - 5 User trying to access Common protected route -> allow
+        if (routerOwner === "COMMON") {
+            return NextResponse.next();
+        }
+
+        //? Rule-6 User trying to visit role based protected but doesn't have required role -> redirect to their default dashboard
+        if (
+            routerOwner === "ADMIN" ||
+            routerOwner === "DOCTOR" ||
+            routerOwner === "PATIENT"
+        ) {
+            if (routerOwner !== userRole) {
+                return NextResponse.redirect(
+                    new URL(
+                        getDefaultDashboardRoute(userRole as UserRole),
+                        request.url,
+                    ),
+                );
+            }
+        }
+
+        return NextResponse.next();
     } catch (error) {
         console.log("Error in proxy middleware", error);
     }
