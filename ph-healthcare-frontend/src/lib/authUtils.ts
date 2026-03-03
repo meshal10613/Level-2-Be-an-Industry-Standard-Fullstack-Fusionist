0919c06d6e1cb8cdc38932586d6e38dbc9334e21
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-03-03 12:36:15 +0600

    protected route config created

--- ph-healthcare-frontend/src/lib/authUtils.ts
+++ ph-healthcare-frontend/src/lib/authUtils.ts
@@ -7,3 +7,37 @@ export const authRoutes = [
     "/reset-password",
     "/verify-email",
 ];
+
+export const isAuthRoute = (pathname: string) => {
+    return authRoutes.some((router: string) => router === pathname);
+};
+
+export type RouteConfig = {
+    exact: string[];
+    pattern: RegExp[];
+};
+
+export const commonProtectedRoutes: RouteConfig = {
+    exact: ["/my-profile", "/change-password"],
+    pattern: [],
+};
+
+export const patientProtectedRoutes: RouteConfig = {
+    exact: ["/payment/success"],
+    pattern: [/^\/dashboard/], // Matches any path that starts with /dashboard
+};
+
+export const doctorProtectedRoutes: RouteConfig = {
+    exact: [],
+    pattern: [/^\/doctor\/dashboard/], // Matches any path that starts with /doctor/dashboard
+};
+
+export const adminProtectedRoutes: RouteConfig = {
+    exact: [],
+    pattern: [/^\/admin\/dashboard/], // Matches any path that starts with /admin/dashboard
+};
+
+export const superAdminProtectedRoutes: RouteConfig = {
+    pattern: [/^\/admin\/dashboard/], // Matches any path that starts with /super-admin/dashboard
+    exact: [],
+};
