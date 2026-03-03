3e87015cb569b191a6b441f3c330b81cf148e322
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-03-03 12:14:21 +0600

    proxy file created

---
+++ ph-healthcare-frontend/src/lib/authUtils.ts
@@ -0,0 +1,9 @@
+export type UserRole = "SUPER_ADMIN" | "ADMIN" | "DOCTOR" | "PATIENT";
+
+export const authRoutes = [
+    "/login",
+    "/register",
+    "/forgot-password",
+    "/reset-password",
+    "/verify-email",
+];
