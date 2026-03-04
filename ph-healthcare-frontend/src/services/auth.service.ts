3aeef79f442f34c5f8b5036fc02b9ba9409674e3
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-03-04 13:17:02 +0600

    feat: add refreshToken function to handle token renewal and cookie management

---
+++ ph-healthcare-frontend/src/services/auth.service.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { setTokenInCookies } from "../lib/tokenUtils";
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+if (!API_BASE_URL) {
+    throw new Error("API_BASE_URL is not defined in environment variables");
+}
+
+export async function refreshToken(refreshToken: string): Promise<boolean> {
+    try {
+        const res = await fetch(`${API_BASE_URL}/auth/refresh-token`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Cookie: `refreshToken=${refreshToken}`,
+            },
+            body: JSON.stringify({ refreshToken }),
+        });
+
+        if (!res.ok) {
+            return false;
+        }
+
+        const { data } = await res.json();
+        const { accessToken, refreshToken: newRefreshToken, token } = data;
+
+        if (accessToken) {
+            await setTokenInCookies("accessToken", accessToken);
+        }
+
+        if (newRefreshToken) {
+            await setTokenInCookies("refreshToken", newRefreshToken);
+        }
+
+        if (token) {
+            await setTokenInCookies(
+                "better-auth.session_token",
+                token,
+                24 * 60 * 60,
+            ); // 1 day in seconds
+        }
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
