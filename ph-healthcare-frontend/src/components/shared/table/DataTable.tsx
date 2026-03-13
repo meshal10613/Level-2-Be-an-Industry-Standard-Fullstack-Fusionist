75ed48b08c45190f058d4ab3fb91dcce08d8f45c
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-03-13 19:01:19 +0600

    feat: enhance DataTable component with action dropdown for view, edit, and delete functionalities

--- ph-healthcare-frontend/src/components/shared/table/DataTable.tsx
+++ ph-healthcare-frontend/src/components/shared/table/DataTable.tsx
@@ -1,4 +1,16 @@
-import { ColumnDef } from "@tanstack/react-table";
+import {
+    ColumnDef,
+    getCoreRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "../../ui/dropdown-menu";
+import { Button } from "../../ui/button";
+import { MoreHorizontal } from "lucide-react";
 
 interface DataTableActions<TData> {
     onView?: (data: TData) => void;
@@ -8,7 +20,7 @@ interface DataTableActions<TData> {
 
 interface DataTableProps<TData> {
     data: TData[];
-    colums: ColumnDef<TData>[];
+    columns: ColumnDef<TData>[];
     actions?: DataTableActions<TData>;
     emptyMessage?: string;
     isLoading?: boolean;
@@ -16,11 +28,76 @@ interface DataTableProps<TData> {
 
 export default function DataTable<TData>({
     data,
-    colums,
+    columns,
     actions,
     emptyMessage,
     isLoading,
 }: DataTableProps<TData>) {
+    const tableColumns: ColumnDef<TData>[] = actions
+        ? 	[
+				...columns,
+				{
+					id: "actions",
+					header: "Actions",
+					cell: ({ row }) => {
+						const rowData = row.original;
+						return (
+							<DropdownMenu>
+								<DropdownMenuTrigger>
+									<Button
+										variant={`ghost`}
+										className="h-8 w8 p-0"
+									>
+										<span>Open Menu</span>
+										<MoreHorizontal className="h-4 w-4" />
+									</Button>
+								</DropdownMenuTrigger>
+
+								<DropdownMenuContent align="end">
+									{actions.onView && (
+										<DropdownMenuItem
+											onClick={() =>
+												actions.onView?.(rowData)
+											}
+										>
+											View
+										</DropdownMenuItem>
+									)}
+
+									{actions.onView && (
+										<DropdownMenuItem
+											onClick={() =>
+												actions.onEdit?.(rowData)
+											}
+										>
+											Edit
+										</DropdownMenuItem>
+									)}
+									
+									{actions.onView && (
+										<DropdownMenuItem
+											onClick={() =>
+												actions.onDelete?.(rowData)
+											}
+										>
+											Delete
+										</DropdownMenuItem>
+									)}
+								</DropdownMenuContent>
+							</DropdownMenu>
+						);
+					},
+				},
+			]
+        : columns;
+
+    // eslint-disable-next-line react-hooks/incompatible-library
+    const table = useReactTable({
+        data,
+        columns: tableColumns,
+        getCoreRowModel: getCoreRowModel(),
+    });
+
     return (
         <div>
             <h1>This is Table Page</h1>
