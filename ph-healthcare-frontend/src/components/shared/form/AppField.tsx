399013503e98d661fd36bdf908baadec7630147e
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-03-17 19:11:41 +0600

    feat: schedule table create with add, edit, view & delete individual schedule

--- ph-healthcare-frontend/src/components/shared/form/AppField.tsx
+++ ph-healthcare-frontend/src/components/shared/form/AppField.tsx
@@ -18,7 +18,7 @@ const getErrorMessage = (error: unknown): string => {
 type AppFieldProps = {
     field: AnyFieldApi;
     label: string;
-    type?: "text" | "email" | "password" | "number";
+    type?: "text" | "email" | "password" | "number" | "date" | "time";
     placeholder?: string;
     append?: React.ReactNode;
     prepend?: React.ReactNode;
